2c95dc6f091932a3187bbcd72a9cf7c6379ed058
YogaDarma1011 <95033531+YogaDarma1011@users.noreply.github.com>
2021-12-07 00:09:31 +0800

    Update bank sampah .c

--- bank sampah .c
+++ bank sampah .c
@@ -4,6 +4,11 @@ Tanggal		: 04/12/2021
 Oleh		: Ni Komang Putri Sandya
 Revisi		: 0
 Deskripsi	: Membuat variabel, fungsi login_admin untuk aktivasi program dengan strcmp, dan fungsi utama
+
+Tanggal		: 06/12/2021
+Oleh		: Putu Agus Yoga Budhi Darma
+Revisi		: 01
+Deskripsi	: Membuat fungsi welcome, login user, buat akun user dan menambahkan fungsi utama
 *******************************************************************************************************************/
 
 #include <stdio.h>
@@ -20,6 +25,7 @@ typedef struct{
 void login_admin (char username [10], char password [10]);
 void welcome ();
 void login_user ();
+void buat_akun_user ();
 
 //deklarasi variabel global
 char username [10], password [10];
@@ -43,6 +49,28 @@ void login_admin (char username [10], char password [10]) {
     	system("cls");
     	login_admin (username, password);
 	}
+}
+
+void welcome (){
+	printf("\n----------------------------------------------------------------------------------------------\n");
+	printf("					SELAMAT DATANG DI");
+	printf("\n			PROGRAM LAYANAN BANK SAMPAH JUMPAI ASRI LESTARI\n");
+	printf("----------------------------------------------------------------------------------------------\n");
+	printf("				MENU LAYANAN : \n");
+}
+
+void login_user (){
+	printf("----------------------------------------------------------------------------------------------\n");
+	printf("					MENU LOGIN USER\n");
+	printf("----------------------------------------------------------------------------------------------\n");
+}
+
+void buat_akun_user (){
+	printf("----------------------------------------------------------------------------------------------\n");
+	printf("					MENU BUAT AKUN USER\n");
+	printf("----------------------------------------------------------------------------------------------\n");
+}
+}
 }
 
 int main () {
